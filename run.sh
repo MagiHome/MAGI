a4adb9fcb0a538312dd571f23bbc301f9d4960e2
MagiHome <5423252@qq.com>
2017-06-02 01:39:03 +0000

    q

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ sudo docker rm hass
 sudo docker stop bridge
 sudo docker rm bridge
 sudo docker run -d -it --name="hass" -v $(pwd)/config:/config -v /etc/localtime:/etc/localtime:ro --net=host magihome/homeassistant:latest
-sudo docker run -d -it --name="bridge" -v $(pwd)/config/config.json:/root/.homebridge/config.json --net=host magihome/homebridge:latest
+sudo docker run -d -it --name="bridge" -v $(pwd)/config/homebridge:/root/.homebridge --net=host magihome/homebridge:latest
