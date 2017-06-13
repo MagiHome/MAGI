fa7c1f24a335760e0b0d07ea98a22ef62ee0cd17
MagiHome <5423252@qq.com>
2017-06-13 13:42:18 +0800

    new airplay-audio image

---
+++ airplay/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+service avahi-daemon stop
+rm -rf /var/run/avahi-daemon/
+rm -rf /var/run/dbus
+mkdir /var/run/dbus
+mkdir /var/run/avahi-daemon/pid
+id avahi
+x=$((9000 + RANDOM % 1000))
+usermod -u $x avahi
+dbus-daemon --system
+sleep 1
+service avahi-daemon start
+sleep 2
+/usr/local/bin/shairport-sync 
--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@ sudo docker stop hass
 sudo docker rm hass
 sudo docker stop bridge
 sudo docker rm bridge
-sudo docker run -d -it --privileged --name="hass" -v $(pwd)/config:/config -v /dev/snd:/dev/snd -v /etc/localtime:/etc/localtime:ro --net=host magihome/homeassistant-dev 
+sudo docker stop airplay
+sudo docker rm airplay
+sudo docker run -d -it --privileged --name="hass" -v $(pwd)/config:/config -v /dev/snd:/dev/snd -v /etc/localtime:/etc/localtime:ro --net=host magihome/homeassistant:latest 
 sudo docker run -d -it --name="bridge" -v $(pwd)/config/homebridge:/root/.homebridge --net=host magihome/homebridge:latest
+sudo docker run -d -it --privileged --name="airplay" -v $(pwd)/config/airplay/shairport-sync.conf:/usr/local/etc/shairport-sync.conf --net=host magihome/airplay:latest
