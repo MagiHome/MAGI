3c1772b825efbe3ee10bcd778bafb87874b1237f
MagiHome <5423252@qq.com>
2017-06-15 15:24:01 +0800

    fix bug for VLC

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ sudo docker stop airplay
 sudo docker rm airplay
 sudo docker run -d -it --privileged --name="hass" -v $(pwd)/config:/config -v /dev/snd:/dev/snd -v /etc/localtime:/etc/localtime:ro --net=host magihome/homeassistant:latest 
 sudo docker run -d -it --name="bridge" -v $(pwd)/config/homebridge:/root/.homebridge --net=host magihome/homebridge:latest
-sudo docker run -d -it --privileged --name="airplay" -v $(pwd)/config/airplay/shairport-sync.conf:/usr/local/etc/shairport-sync.conf --net=host magihome/airplay:latest
+sudo docker run -d -it --privileged --name="airplay" --device=/dev/snd:/dev/snd -v $(pwd)/config/airplay/shairport-sync.conf:/usr/local/etc/shairport-sync.conf --net=host magihome/airplay:latest
